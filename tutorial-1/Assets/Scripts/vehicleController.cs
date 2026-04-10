ca7c502a7d0fa6f44fc931d4318e27af164fe3f5
Joseph Coulson <jpcoulso@ucsc.edu>
2026-04-10 00:54:02 -0700

    added horizontal movement, mouse steering temp commented out

--- tutorial-1/Assets/Scripts/vehicleController.cs
+++ tutorial-1/Assets/Scripts/vehicleController.cs
@@ -4,10 +4,12 @@ using UnityEngine.InputSystem;
 public class vehicleController : MonoBehaviour
 {
     float desired_acceleration;
+    float desired_strafe;
     void OnMove(InputValue action)
     {
         var movement = action.Get<Vector2>();
         desired_acceleration = movement.y;
+        desired_strafe = movement.x;
     }
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,10 +22,11 @@ public class vehicleController : MonoBehaviour
     void Update()
     {
         GetComponent<Rigidbody>().AddRelativeForce(desired_acceleration*8, 0, 0);
-        float dx = (Mouse.current.position.x.value - Screen.width / 2) / 200;
-        if (Mathf.Abs(dx) > 0.01f)
-        {
-            transform.Rotate(0, dx, 0);
-        }
+        GetComponent<Rigidbody>().AddRelativeForce(0, 0, -desired_strafe*8);
+        //float dx = (Mouse.current.position.x.value - Screen.width / 2) / 200;
+        //if (Mathf.Abs(dx) > 0.01f)
+        //{
+            //transform.Rotate(0, dx, 0);
+        //}
     }
 }
